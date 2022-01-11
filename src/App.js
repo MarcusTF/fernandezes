995488e00b3a7457bbaf3a77fc12c538e89e79bd
Marcus Fernandez <marcustfernandez@gmail.com>
2022-01-11 16:59:36 -0500

    added stops fetching to context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import { MapContainer } from "./components/MapContainer"
 
 import "./App.scss"
+import { Header } from "./components/Header"
+import { Loading } from "./components/Loading"
+import { useContext, useEffect } from "react"
+import { MapContext } from "./context/map"
 
 function App() {
+  const {
+    getStops,
+    stops: { loading },
+  } = useContext(MapContext)
+
+  useEffect(() => getStops(), [getStops])
+
   return (
     <div className='App'>
+      {loading && <Loading />}
+      <Header />
       <MapContainer />
     </div>
   )
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import "./Header.scss"
+
+const Header = () => {
+  return <header>The Fernandezes</header>
+}
+
+export default Header
