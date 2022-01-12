1977fcfe331a3c9fa69e2ceea48648e8abc3f4cd
Marcus Fernandez <marcustfernandez@gmail.com>
2022-01-11 19:31:42 -0500

    merge

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import { MapContainer } from "./components/MapContainer"
 
 import "./App.scss"
+import { useContext, useEffect } from "react"
+import { MapContext } from "./context/map"
 
 function App() {
+  const {
+    getStops,
+    stops: { loading },
+  } = useContext(MapContext)
+
+  useEffect(() => getStops(), [getStops])
+
+  // const loading = true
+
   return (
     <div className='App'>
       <MapContainer />
