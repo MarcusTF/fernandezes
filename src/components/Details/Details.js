6b2a85b49178c48d8a6e29d5a89805a925626a4f
Marcus Fernandez <marcustfernandez@gmail.com>
2022-01-15 15:46:53 -0500

    temporary about panel

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import Lottie from "react-lottie-player"
 import { loadingLottieData } from "../../assets/lottie"
+import { XIcon } from "../../assets/vector"
 
 import { DetailsContext } from "../../context"
 import { prettyCoords } from "../../utils/utils"
@@ -11,6 +12,7 @@ const Details = () => {
   const {
     panel,
     stop: { data: stop, loading, error },
+    closePanel,
   } = useContext(DetailsContext)
 
   return stop || panel || loading ? (
@@ -25,29 +27,32 @@ const Details = () => {
             className='loading-lottie'
           />
         </div>
+      ) : panel ? (
+        <div className='panel-wrapper'>
+          <XIcon role='button' aria-label='clear search' className='close' onClick={() => closePanel()} />
+          {panel}
+        </div>
       ) : (
-        panel || (
-          <>
-            <div className='details-title'>
-              <h2>{stop?.title?.split(",")[0]}</h2>
-              <div className='address-coords-wrapper'>
-                <p className='coordinates'>{prettyCoords(stop)}</p>
-                <p className='address'>{stop?.location?.address || stop?.title}</p>
-              </div>
+        <>
+          <div className='details-title'>
+            <h2>{stop?.title?.split(",")[0]}</h2>
+            <div className='address-coords-wrapper'>
+              <p className='coordinates'>{prettyCoords(stop)}</p>
+              <p className='address'>{stop?.location?.address || stop?.title}</p>
             </div>
-            {stop?.images?.thumbnail?.mediaItemUrl ? (
-              <div className='details-image'>
-                <img src={stop?.images?.thumbnail?.mediaItemUrl} alt='featured' />
-              </div>
-            ) : null}
-            {stop?.text?.description ? (
-              <div className='details-description'>
-                {stop?.text?.title ? <h3 className='title'>{stop?.text?.title}</h3> : null}
-                <p className='description'>{stop?.text?.description}</p>
-              </div>
-            ) : null}
-          </>
-        )
+          </div>
+          {stop?.images?.thumbnail?.mediaItemUrl ? (
+            <div className='details-image'>
+              <img src={stop?.images?.thumbnail?.mediaItemUrl} alt='featured' />
+            </div>
+          ) : null}
+          {stop?.text?.description ? (
+            <div className='details-description'>
+              {stop?.text?.title ? <h3 className='title'>{stop?.text?.title}</h3> : null}
+              <p className='description'>{stop?.text?.description}</p>
+            </div>
+          ) : null}
+        </>
       )}
     </div>
   ) : null
