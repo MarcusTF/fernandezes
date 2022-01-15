c757c691daa90404874557d7cdbadf32e90e86a5
Marcus Fernandez <marcustfernandez@gmail.com>
2022-01-15 15:46:53 -0500

    temporary about panel

--- src/components/Panels/About/About.js
+++ src/components/Panels/About/About.js
@@ -5,10 +5,27 @@ import "./About.scss"
 const About = () => {
   const { closePanel } = useContext(DetailsContext)
   return (
-    <info>
-      <p>HELLO WORLD</p>
-      <button onClick={() => closePanel()}>X</button>
-    </info>
+    <div className='about'>
+      <h3>About</h3>
+      <h4>What is this?</h4>
+      <p>
+        This site is a way for friends, family, and anybody who's interested to keep up with our adventures as we travel
+        the world.
+      </p>
+      <h5>Special Thanks:</h5>
+      <ul>
+        <li>
+          <a target='_blank' rel='noreferrer' href='https://pigeon-maps.js.org/'>
+            Pigeon Maps
+          </a>
+        </li>
+        <li>
+          <a target='_blank' rel='noreferrer' href='https://www.openstreetmap.org/'>
+            OpenStreetMaps
+          </a>
+        </li>
+      </ul>
+    </div>
   )
 }
 
