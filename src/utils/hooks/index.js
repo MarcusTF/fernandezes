f56b08f4ec334ce6fec99d24fb89774f847b7d3e
Marcus Fernandez <marcustfernandez@gmail.com>
2022-01-27 23:23:37 -0500

    log user out if refresh token is over 7 days old.

--- src/utils/hooks/index.js
+++ src/utils/hooks/index.js
@@ -1,51 +1,5 @@
-import dayjs from "dayjs"
-import { Marker } from "pigeon-maps"
-import { useCallback, useContext, useEffect, useRef, useState, useLayoutEffect } from "react"
+import { useContext, useEffect, useLayoutEffect, useRef, useState } from "react"
 import { AuthContext } from "../../context"
-import { MapContext } from "../../context/map"
-
-export const useGenerateMarkers = stops => {
-  const { setMap } = useContext(MapContext)
-
-  const colorPicker = data => {
-    if (data?.when === "next") return "#ffb05c"
-    else if (data?.startDate && data?.endDate) {
-      const startDate = dayjs(data?.startDate, "MM/DD/YYYY").valueOf(),
-        endDate = dayjs(data?.endDate, "MM/DD/YYYY").valueOf(),
-        today = dayjs().valueOf()
-      if (endDate < today) return "#c20e0e"
-      if (startDate < today && endDate > today) return "#fffc5c"
-      if (startDate > today) return "#42b52b"
-      return "#c2c2c2"
-    } else if (data?.date) {
-      const date = dayjs(data?.startDate, "MM/DD/YYYY").valueOf(),
-        today = dayjs().valueOf()
-      if (date < today) return "#c20e0e"
-      if (date > today) return "#42b52b"
-    } else if (data?.when)
-      switch (data?.when) {
-        case "past":
-          return "#c20e0e"
-        case "current":
-          return "#fffc5c"
-        case "future":
-          return "#42b52b"
-        default:
-          return "#c2c2c2"
-      }
-  }
-
-  const handleClick = useCallback(e => setMap(e.anchor, 11), [setMap])
-
-  return stops?.map?.(stop => (
-    <Marker
-      onClick={handleClick}
-      key={stop?.id}
-      color={colorPicker(stop?.time)}
-      anchor={[stop?.location?.coords?.lat, stop?.location?.coords?.lng]}
-    />
-  ))
-}
 
 /**
  * Simple debouncing using useEffect.
